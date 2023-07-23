ac5e56bb321963578a028709238279f1369d1f46
Kim Hyeonseok <kim010322@gmail.com>
2023-07-23 14:30:12 +0900

    초기 구동 확인

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import cv2
 
+# 실행 시 장치 번호 확인하기
+DEVICE_INDEX = 0
+capture = cv2.VideoCapture(DEVICE_INDEX)
 
-# load lenna image
-img = cv2.imread('lenna.png')
+_ret, frame = capture.read()
 
-# display image
-cv2.imshow('Lenna', img)
-cv2.waitKey()
-cv2.destroyAllWindows()
+# 비디오 입력 테스트
+# Expected: (480, 640, 3)
+print(f"비디오 크기: {frame.shape}")
+
+while True:
+    ret, frame = capture.read()
+
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
