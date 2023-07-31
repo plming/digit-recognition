1da6375377ee0ae8cd80603e8b789c821df88e3d
Kim Hyeonseok <kim010322@gmail.com>
2023-07-31 16:52:29 +0900

    전처리 단계 오류 수정

--- main.py
+++ main.py
@@ -1,37 +1,42 @@
 import cv2
 import numpy as np
+from tensorflow import keras
 from keras.models import load_model
-
-WIDTH, HEIGHT = 640, 480
 
 model = load_model('model.keras', compile=False)
 
-capture = cv2.VideoCapture(0)
+capture = cv2.VideoCapture(1)
+
+width = int(capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
 while True:
     ret, frame = capture.read()
 
-    # TODO: 임시 해상도 조정을 위한 코드로 실 환경에선 제거해야 함
-    frame = cv2.resize(src=frame, dsize=(WIDTH, HEIGHT), interpolation=cv2.INTER_AREA)
+    box_size = height // 2
+    left_top = (width // 2 - box_size // 2, height // 2 - box_size // 2)
+    right_bottom = (width // 2 + box_size // 2, height // 2 + box_size // 2)
 
-    box_size = HEIGHT // 2
-    left_top = (WIDTH // 2 - box_size // 2, HEIGHT // 2 - box_size // 2)
-    right_bottom = (WIDTH // 2 + box_size // 2, HEIGHT // 2 + box_size // 2)
+    roi = frame[left_top[1]:right_bottom[1], left_top[0]:right_bottom[0]].copy()
 
     GREEN = (0, 255, 0)
     cv2.rectangle(frame, left_top, right_bottom, GREEN, 2)
 
-    roi = frame[left_top[1]:right_bottom[1], left_top[0]:right_bottom[0]]
-
-    x = cv2.resize(roi, (28, 28), interpolation=cv2.INTER_AREA)
+    x = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+    x = cv2.resize(x, (28, 28), interpolation=cv2.INTER_AREA)
+    x = cv2.bitwise_not(x)
     x = np.expand_dims(x, axis=0)
 
     pred = model.predict(x, verbose=0)
-    print(f"인식된 숫자는 {np.argmax(pred)}입니다.")
+    digit = np.argmax(pred)
+    print(f"인식된 숫자는 {digit}입니다.")
+
+    # display (digit, percentage) on the frame
+    cv2.putText(frame, f"{digit} ({pred[0][digit] * 100:.2f}%)", (left_top[0], left_top[1] - 10),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.7, GREEN, 2)
 
     cv2.imshow('Video', frame)
-    cv2.imshow('ROI', roi)
-    cv2.imshow('Resized ROI', x[0])
+    cv2.imshow('ROI', cv2.resize(x[0], (200, 200)))
 
     if cv2.waitKey(1) == ord('q'):
         break
