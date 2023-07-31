d51ebc146570af1d4d23ee3824894a59596155e2
Kim Hyeonseok <kim010322@gmail.com>
2023-07-31 21:10:18 +0900

    code refactoring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import cv2
 import numpy as np
-from tensorflow import keras
 from keras.models import load_model
 
 model = load_model('model.keras', compile=False)
@@ -10,17 +9,17 @@ capture = cv2.VideoCapture(1)
 width = int(capture.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
+box_size = height // 2
+left, top = (width // 2 - box_size // 2, height // 2 - box_size // 2)
+right, bottom = (width // 2 + box_size // 2, height // 2 + box_size // 2)
+
 while True:
     ret, frame = capture.read()
 
-    box_size = height // 2
-    left_top = (width // 2 - box_size // 2, height // 2 - box_size // 2)
-    right_bottom = (width // 2 + box_size // 2, height // 2 + box_size // 2)
-
-    roi = frame[left_top[1]:right_bottom[1], left_top[0]:right_bottom[0]].copy()
+    roi = frame[top:bottom, left:right]
 
     GREEN = (0, 255, 0)
-    cv2.rectangle(frame, left_top, right_bottom, GREEN, 2)
+    cv2.rectangle(frame, (left, top), (right, bottom), GREEN, 2)
 
     x = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
     x = cv2.resize(x, (28, 28), interpolation=cv2.INTER_AREA)
@@ -29,14 +28,18 @@ while True:
 
     pred = model.predict(x, verbose=0)
     digit = np.argmax(pred)
-    print(f"인식된 숫자는 {digit}입니다.")
+    probability = pred[0][digit]
 
-    # display (digit, percentage) on the frame
-    cv2.putText(frame, f"{digit} ({pred[0][digit] * 100:.2f}%)", (left_top[0], left_top[1] - 10),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.7, GREEN, 2)
+    cv2.putText(frame,
+                f"{digit} ({probability * 100:.2f}%)",
+                (left, top - 10),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.8,
+                GREEN,
+                2)
 
-    cv2.imshow('Video', frame)
-    cv2.imshow('ROI', cv2.resize(x[0], (200, 200)))
+    cv2.imshow('frame', frame)
+    cv2.imshow('x', cv2.resize(x[0], (200, 200)))
 
     if cv2.waitKey(1) == ord('q'):
         break
